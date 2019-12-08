aa884bdfb81d49c28693f56321a31d392928a3f7
ahctam <donkeynotlonely@gmail.com>
2019-12-08 19:28:27 +0800

    Taiwan plus America

--- driving.py
+++ driving.py
@@ -5,3 +5,10 @@ if country == '臺灣' or country == '台灣':
 		print('您已符合駕照報考資格。')
 	else:
 		print('您尚未符合駕照報考資格。')
+elif country == '美國':
+	if int(age) >= 16:
+		print('您已符合駕照報考資格。')
+	else:
+		print('您尚未符合駕照報考資格。')
+else:
+	print('請確認您輸入的國籍符合範圍。')
